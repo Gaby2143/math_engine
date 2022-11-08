a6f05e6770f0093ab0482e46d3a0dfaf801fbebb
AndreiDimofte <78376776+AndreiDimofte@users.noreply.github.com>
2022-11-08 19:09:34 +0200

    Cmmdc + cmmmc

--- Math_engine/Math.cpp
+++ Math_engine/Math.cpp
@@ -3,3 +3,16 @@ bool Math::Is_even(long long int number)
 {
 	return number % 2 == 0 ? true : false;
 }
+int Math::Cmmdc(int a, int b)
+{
+	while (a!=b)
+	{
+		if (a > b) a -= b;
+		else b -= a;
+	}
+	return a;
+}
+int Math::Cmmmc(int a, int b)
+{
+	return a * b / Cmmdc(a, b);
+}
